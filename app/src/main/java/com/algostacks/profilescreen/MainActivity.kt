14b10b95d50ff43ef14af8c770087973df0524a9
pelumzeny <pelumzeny10@gmail.com>
2018-09-06 02:37:32 +0100

    first commit

---
+++ app/src/main/java/com/algostacks/profilescreen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.algostacks.profilescreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.material_design_profile_screen_xml_ui_design)
+    }
+}
